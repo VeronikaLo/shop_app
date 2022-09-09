990bf7e454f2cee2830a6487f3c210f09e5518f8
Veronika L <verokipri@yahoo.com>
2022-09-09 10:09:11 +0200

    finish product detail page

--- lib/screens/prod_detail_screen.dart
+++ lib/screens/prod_detail_screen.dart
@@ -12,6 +12,30 @@ class ProductDetailScreen extends StatelessWidget {
     final loadedProduct = Provider.of<Products>(context).findById(prodId);
     return Scaffold(
       appBar: AppBar(title:  Text(loadedProduct.title)),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              width: double.infinity,
+              height: 300,
+              child: Image.network(loadedProduct.imageUrl, fit: BoxFit.cover,),),
+            const SizedBox(height: 20,),
+            Text(
+              '\$ ${loadedProduct.price}', 
+              style: const TextStyle(fontSize: 22) ,),
+            const SizedBox(height: 25,),
+            Container(
+              width: double.infinity,
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Text(
+                loadedProduct.description, 
+                style: const TextStyle(fontSize: 22),
+                textAlign: TextAlign.center,
+                softWrap: true,),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
